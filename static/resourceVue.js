f180d5579ad8b1ca7c4ec76f78e6fedc12bd8907
Brendan <dayleycoders@gmail.com>
2021-02-07 12:00:36 -0700

    Vue inital update

---
+++ static/resourceVue.js
@@ -0,0 +1,33 @@
+let VueAdderClass = new Vue({
+    el: '#VueResource',
+    template: `<div >
+                    <label>{{type}}</label>
+                    <input type="number" value="{{priority}}">
+                </div>`,
+    data: {
+        // resources: [
+        //     {
+        //     resource: {
+                type: wood,
+                priority: 1
+            //   }
+            // }
+        // ]
+
+    },
+    methods: {
+        add: function(){
+            fetch("/add2?x="+this.x+"&y="+this.y)
+                .then(response => response.json())
+                .then(results => (this.value = JSON.stringify(results.value)))
+        },
+        addAsync: async function(){
+            const response = await fetch("/add2?x="+this.x+"&y="+this.y);
+            const results = await response.json();
+            console.log(JSON.stringify(results));
+            this.value = JSON.stringify(results);
+        }
+    }
+  });
+
+export const VueAdder = VueAdderClass;
--- uuid.py
+++
@@ -1,5 +0,0 @@
-import uuid
-from enum import Enum
-
-def generateID():
-    return str(uuid.uuid4()).replace('-','')
