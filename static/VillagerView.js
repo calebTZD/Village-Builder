b8437701ac6fe0269205f4f3bb9719497ea8b604
brad <bwdayley@gmail.com>
2021-05-14 14:50:38 -0600

    Fixed VillagerView

--- static/VillagerView.js
+++ static/VillagerView.js
@@ -13,16 +13,16 @@ export const VilligerView = {
                             <label>Spawn Time: </label><input type="number" v-model="item.settings.spawnTime">
                         </div>
                         <h6> Spawn Cost: </h6>
-                        <div v-for="(cost, type) in item.settings.spawnCost">
-                            {{type}}:  
-                            <input type="number" v-model="cost">
+                        <div v-for="(cost, typea) in item.settings.spawnCost">
+                            {{typea}}:  
+                            <input type="number" v-model="item.settings.spawnCost[typea]">
                         </div>
                     </div>
                     <br>
-                </div>`,
+                </div>,
     data() {
         return {
-            villagerData: {'type':{'settings':{'speed': 1, 'maxHealth': 100, 'enhancemntCost': 200, 'carryCapacity': 1, 'attack': 1, 'defense': 1, 'productionSpeed': 1, 'spawnTime': 1, 'spawnCost':{'food': 50}}}}
+            villagerData: {'type':{'settings':{'speed': 1, 'maxHealth': 100, 'enhancemntCost': 200, 'carryCapacity': 1, 'attack': 1, 'defense': 1, 'productionSpeed': 1, 'spawnTime': 1, 'spawnCost':{'wood': 60}}}}
         }
     },    
     methods: {    
