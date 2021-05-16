6fb5d76cb0345236e49496bc41a32a940334fb93
brad <bwdayley@gmail.com>
2021-05-16 14:57:53 -0600

    cleanup root

--- Resource.py
+++
@@ -1,8 +0,0 @@
-class resource:
-    def __init__(self, type, rp = 500):
-        self.type = type
-        self.rPoints = rp
-
-    def reduceRP(self, amount):
-        self.rPoints -= amount
-        
