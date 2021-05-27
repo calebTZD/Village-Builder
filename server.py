668255b3bfa44b653dd8c2f47c07f68a214e72fe
brad <bwdayley@gmail.com>
2021-05-27 16:17:03 -0600

    Server APIs

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, make_response, render_template, redirect, url_for, after_this_request, jsonify
 from flask_socketio import SocketIO
-import traceback, json
+import sys, traceback, json
 from bson import json_util
 
 
@@ -28,22 +28,183 @@ gamesocket = SocketIO(app)
 def index():
     return redirect(url_for('static', filename='Simulation.html'))
 
-@app.route('/getData')
-def getResource():
-    data = SimData.getByName() 
+@app.route('/getSim', methods = ['GET'])
+def getSim():
+    try:
+        simName = request.args.get('simName')
+        sim = SimData.getByName(simName)
+        return json.dumps(sim)
+    except:
+        traceback.print_exc(file=sys.stdout)
+        raise InvalidUsageExeption("Failed to update Villages.", status_code=400)
 
-    return serializeToJSON(data)
+@app.route('/createSim', methods = ['GET'])
+def createSim():
+    try:
+        simName = request.args.get('simName')
+        SimData.create(simName)
+        sim = SimData.getByName(simName)
+        return json.dumps(sim)
+    except:
+        traceback.print_exc(file=sys.stdout)
+        raise InvalidUsageExeption("Failed to update Villages.", status_code=400)
 
-@app.route('/getVillages')
+@app.route('/deletSim', methods = ['GET'])
+def deletSim():
+    try:
+        simName = request.args.get('simName')
+        results = SimData.delete(simName)
+        if results == True:
+            return "Success"
+        else:
+            raise Exception("Failure")    
+    except:
+        traceback.print_exc(file=sys.stdout)
+        raise InvalidUsageExeption("Failed to update Villages.", status_code=400)
+
+@app.route('/getWorld', methods = ['GET'])
+def getWorld():
+    try:
+        simName = request.args.get('simName')
+        world = SimData.getWorld(simName)
+        return json.dumps(world)
+    except:
+        traceback.print_exc(file=sys.stdout)
+        raise InvalidUsageExeption("Failed to update Villages.", status_code=400)
+
+@app.route('/updateWorld', methods = ['POST'])
+def updateWorld():
+    try:
+        simName = request.args.get('simName')
+        world = json.loads(request.data)
+        results = SimData.updateWorld(simName, world)
+        if results == True:
+            return "Success"
+        else:
+            raise Exception("Failure")
+    except:
+        traceback.print_exc(file=sys.stdout)
+        raise InvalidUsageExeption("Failed to update Villages.", status_code=400)
+
+@app.route('/getVillages', methods = ['GET'])
 def getVillages():
-    #data = villageData.getInitalResources() #TODO: remove hardcoded data
-    f = open("iDataVillages.json","r")
-    lines = f.readlines()
-    sData = ""
-    for line in lines:
-        sData += line.rstrip()
-    villages = json.loads(sData)
-    return json.dumps(villages)
+    try:
+        simName = request.args.get('simName')
+        villages = SimData.getVillages(simName)
+        return json.dumps(villages)
+    except:
+        traceback.print_exc(file=sys.stdout)
+        raise InvalidUsageExeption("Failed to update Villages.", status_code=400)
+
+@app.route('/updateVillages', methods = ['POST'])
+def updateVillages():
+    try:
+        simName = request.args.get('simName')
+        villages = json.loads(request.data)
+        results = SimData.updateVillages(simName, villages)
+        if results == True:
+            return "Success"
+        else:
+            raise Exception("Failure")
+    except:
+        traceback.print_exc(file=sys.stdout)
+        raise InvalidUsageExeption("Failed to update Villages.", status_code=400)
+
+@app.route('/getVillagers', methods = ['GET'])
+def getVillagers():
+    try:
+        simName = request.args.get('simName')
+        villagers = SimData.getVillagers(simName)
+        return json.dumps(villagers)
+    except:
+        traceback.print_exc(file=sys.stdout)
+        raise InvalidUsageExeption("Failed to update Villages.", status_code=400)
+
+@app.route('/updateVillagers', methods = ['POST'])
+def updateVillagers():
+    try:
+        simName = request.args.get('simName')
+        villagers = json.loads(request.data)
+        results = SimData.updateVillagers(simName, villagers)
+        if results == True:
+            return "Success"
+        else:
+            raise Exception("Failure")
+    except:
+        traceback.print_exc(file=sys.stdout)
+        raise InvalidUsageExeption("Failed to update Villages.", status_code=400)
+
+@app.route('/getLocations', methods = ['GET'])
+def getLocations():
+    try:
+        simName = request.args.get('simName')
+        locations = SimData.getLocations(simName)
+        return json.dumps(locations)
+    except:
+        traceback.print_exc(file=sys.stdout)
+        raise InvalidUsageExeption("Failed to update Villages.", status_code=400)
+
+@app.route('/updateLocations', methods = ['POST'])
+def updateLocations():
+    try:
+        simName = request.args.get('simName')
+        locations = json.loads(request.data)
+        results = SimData.updateLocations(simName, locations)
+        if results == True:
+            return "Success"
+        else:
+            raise Exception("Failure")
+    except:
+        traceback.print_exc(file=sys.stdout)
+        raise InvalidUsageExeption("Failed to update Villages.", status_code=400)
+
+@app.route('/getBuildings', methods = ['GET'])
+def getBuildings():
+    try:
+        simName = request.args.get('simName')
+        buildings = SimData.getBuildings(simName)
+        return json.dumps(buildings)
+    except:
+        traceback.print_exc(file=sys.stdout)
+        raise InvalidUsageExeption("Failed to update Villages.", status_code=400)
+
+@app.route('/updateBuildings', methods = ['POST'])
+def updateBuildings():
+    try:
+        simName = request.args.get('simName')
+        building = json.loads(request.data)
+        results = SimData.updateBuildings(simName, building)
+        if results == True:
+            return "Success"
+        else:
+            raise Exception("Failure")
+    except:
+        traceback.print_exc(file=sys.stdout)
+        raise InvalidUsageExeption("Failed to update Villages.", status_code=400)
+
+@app.route('/getAllVillages')
+def getAllVillages():
+    try:
+        villages = SimData.getAllVillages()
+        return json.dumps(villages)
+    except:
+        traceback.print_exc(file=sys.stdout)
+        raise InvalidUsageExeption("Failed to update Villages.", status_code=400)
+
+@app.route('/updateVillage', methods = ['POST'])
+def updateVillage():
+    try:
+        simName = request.args.get('simName')
+        vData = json.loads(request.data)
+        results = SimData.updateVillage(simName, vData)
+        if results == True:
+            return "Success"
+        else:
+            raise Exception("Failure")
+    except:
+        traceback.print_exc(file=sys.stdout)
+        raise InvalidUsageExeption("Failed to update Villages.", status_code=400)
+
 
 class InvalidUsageExeption(Exception):
     def __init__(self, message, status_code=400, data={}):
